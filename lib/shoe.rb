40e28b8dcb9f6da7e06bf4cb406b9420cb78bd7d
George Talnagi <gtalnagi@yahoo.com>
2020-08-05 21:44:11 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,13 +2,21 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
+BRANDS = []
+
   def initialize(brand)
     @brand = brand
-  end
+    #if BRANDS does not include the brand argument in the list, add it 
+    #if BRANDS does include the argument, skip 
+    #! makes it look for a boolian value of false (! = not that)
+    if !BRANDS.include? (brand)
+      BRANDS << brand 
+    end 
+  end 
 
   def cobble
     self.condition = "new"
     puts "Your shoe is as good as new!"
   end
-
-end
+  
+end 
